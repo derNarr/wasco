964fff0a75a33634531e97fd0e56a2fff21a5b3b
Konstantin Sering <konstantin.sering@gmail.com>
2010-11-08 09:15:10 +0100

    create small dummy

--- wasco.py
+++ wasco.py
@@ -5,7 +5,7 @@
 # (c) 2010 Konstantin Sering <konstantin.sering [aet] gmail.com>
 # GPL 3.0+ or (cc) by-sa (http://creativecommons.org/licenses/by-sa/3.0/)
 #
-# last mod 2010-04-20, KS
+# last mod 2010-11-07, KS
 
 
 """
@@ -18,29 +18,51 @@ boardInfo.
 # Grundsaetzlich ist aus der wasco.h einige Definitionen uebernommen, dann
 # wird mit ctypes die dll geladen und die Karte vorbereitet.
 
-from ctypes import * # TODO reduce *-import
+from ctypes import c_int,c_ulong,byref
 import WascoConstants
+from exceptions import OSError, ImportError
+
+try:
+    from ctypes import windll
+
+    ## load dll
+    wasco = windll.wasco
+    
+    # define some important variables
+    boardId = c_int(1)
+    error = c_ulong()
+    boardInfo = WascoConstants.WascoBoardInfo()
+    
+    ## error, if the board-id is not valid
+    error = wasco.wasco_getBoardInfo(boardId, byref(boardInfo))
+    
+    if( error ):
+        print error
+    
+    ## warning, if there are no analog outputs
+    if( not boardInfo.nAnalogOut ):
+        print "WARNING: no analog outputs found"
+    
+    ## initialize the card
+    wasco.wasco_openBoard( byref(boardId), boardInfo.pBoardName)
+except (OSError, ImportError):
+    class Wasco(object):
+        """
+        Small class to create slim wasco dummy.
+        """
+
+        def __init__(self):
+            pass
+
+        def wasco_outportW(self, board_id, channel, value):
+            """
+            dummy function
+            """
+            pass
+    wasco = Wasco()
+    boardId = c_int(1)
+        
 
 
-## load dll
-wasco = windll.wasco
-
-# define some important variables
-boardId = c_int(1)
-error = c_ulong()
-boardInfo = WascoConstants.WascoBoardInfo()
-
-## error, if the board-id is not valid
-error = wasco.wasco_getBoardInfo(boardId, byref(boardInfo))
-
-if( error ):
-    print error
-
-## warning, if there are no analog outputs
-if( not boardInfo.nAnalogOut ):
-    print "WARNING: no analog outputs found"
-
-## initialize the card
-wasco.wasco_openBoard( byref(boardId), boardInfo.pBoardName)
 
 
