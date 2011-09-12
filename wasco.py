24e9e52a5f59f1a3f2cecddd78cdd0beee36bf17
Nora Umbach <nora.umbach@web.de>
2011-09-12 15:47:03 +0200

    better English and typos

--- wasco.py
+++ wasco.py
@@ -4,19 +4,17 @@
 #
 # (c) 2010 Konstantin Sering <konstantin.sering [aet] gmail.com>
 # GPL 3.0+ or (cc) by-sa (http://creativecommons.org/licenses/by-sa/3.0/)
-#
-# last mod 2010-11-07, KS
 
 
 """
-wasco.py gives you an object wasco which is a handle for the wasco-card
-and allready initialized.
+wasco.py gives you an object wasco which is a handle for wasco card
+and already initialized.
 It also gives you the boardId and boardInfo in the variables boardId and 
 boardInfo.
 """
 
-# Grundsaetzlich ist aus der wasco.h einige Definitionen uebernommen, dann
-# wird mit ctypes die dll geladen und die Karte vorbereitet.
+# Grundsaetzlich werden aus der wasco.h einige Definitionen uebernommen,
+# dann wird mit ctypes die dll geladen und die Karte vorbereitet.
 
 from ctypes import c_int,c_ulong,byref
 import WascoConstants
@@ -33,17 +31,17 @@ try:
     error = c_ulong()
     boardInfo = WascoConstants.WascoBoardInfo()
     
-    ## error, if the board-id is not valid
+    ## error if board-id is not valid
     error = wasco.wasco_getBoardInfo(boardId, byref(boardInfo))
     
     if( error ):
         print error
     
-    ## warning, if there are no analog outputs
+    ## warning if there are no analog outputs
     if( not boardInfo.nAnalogOut ):
         print "WARNING: no analog outputs found"
     
-    ## initialize the card
+    ## initialize card
     wasco.wasco_openBoard( byref(boardId), boardInfo.pBoardName)
 except (OSError, ImportError):
     class Wasco(object):
