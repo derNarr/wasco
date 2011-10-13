9eaa1e9fe3dc5c1fcac7b7264bff6a2baca2a61d
Dominik Wabersich <wabersich@gmx.net>
2011-10-13 22:16:31 +0200

    implemented wasco as class

--- wasco.py
+++ wasco.py
@@ -18,47 +18,57 @@ boardInfo.
 
 from ctypes import c_int,c_ulong,byref
 import WascoConstants
-from exceptions import OSError, ImportError
+from exceptions import OSError, ImportError, BaseException 
 
-try:
-    from ctypes import windll
+class Wasco(object): 
+    """ 
+    Class for wasco.
+    """
+    def __init__(self, dummy=False):
+    try: 
+        if self.dummy is True:
+            raise(BaseException) # GOTO: Exception is just used to jump to the except block
+        from ctypes import windll
+        ## load dll
+        self.wasco = windll.wasco
+        
+        # define some important variables
+        self.boardId = c_int(1)
+        self.error = c_ulong()
+        self.boardInfo = WascoConstants.WascoBoardInfo()
+        
+        ## error if board-id is not valid
+        self.error = self.wasco.wasco_getBoardInfo(self.boardId, byref(self.boardInfo))
+        
+        if( self.error ):
+            print self.error
+        
+        ## warning if there are no analog outputs
+        if( not self.boardInfo.nAnalogOut ):
+            print "WARNING: no analog outputs found"
+        
+        ## initialize card
+        self.wasco.wasco_openBoard( byref(self.boardId), self.boardInfo.pBoardName)
 
-    ## load dll
-    wasco = windll.wasco
-    
-    # define some important variables
-    boardId = c_int(1)
-    error = c_ulong()
-    boardInfo = WascoConstants.WascoBoardInfo()
-    
-    ## error if board-id is not valid
-    error = wasco.wasco_getBoardInfo(boardId, byref(boardInfo))
-    
-    if( error ):
-        print error
-    
-    ## warning if there are no analog outputs
-    if( not boardInfo.nAnalogOut ):
-        print "WARNING: no analog outputs found"
-    
-    ## initialize card
-    wasco.wasco_openBoard( byref(boardId), boardInfo.pBoardName)
-except (OSError, ImportError):
-    class Wasco(object):
+        self.wasco_outportW = self.wasco.wasco_outportW
+
+    except (OSError, ImportError, BaseException):
+        if self.dummy is False:
+            print('''########## WARNING ##########
+                    Cannot load wasco.dll. Creating wasco dummy!
+                    ########## WARNING ##########''')
+        pass
+
+        # dummy for the only function we use
+    def wasco_outportW(self, board_id, channel, value):
         """
-        Small class to create slim wasco dummy.
+        dummy function
         """
+        # only called when dummy=True
+        pass
 
-        def __init__(self):
-            pass
-
-        def wasco_outportW(self, board_id, channel, value):
-            """
-            dummy function
-            """
-            pass
-    wasco = Wasco()
-    boardId = c_int(1)
+wasco = Wasco()
+boardId = c_int(1)
         
 
 
