9d4cad25c898518a8fd94cf21fd53fb6eae0b66f
Konstantin Sering <konstantin.sering@gmail.com>
2012-05-31 22:28:49 +0200

    RF better print statements

--- wasco.py
+++ wasco.py
@@ -13,8 +13,10 @@
 # output: --
 #
 # created 2010
-# last mod 2012-05-30 10:42 DW
+# last mod 2012-05-31 13:07 KS
 
+from __future__ import print_function
+import sys
 from ctypes import c_int,c_ulong,byref
 import constants
 from exceptions import OSError, ImportError, BaseException 
@@ -29,6 +31,7 @@ class Wasco(object):
 
     In principle, some definitions from wasco.h are taken, then dll is
     loaded using ctypes, and card is initialized.
+
     """
     def __init__(self, dummy=False):
         self.dummy = dummy
@@ -48,11 +51,11 @@ class Wasco(object):
             self.error = self.wasco.wasco_getBoardInfo(self.boardId, byref(self.boardInfo))
             
             if( self.error ):
-                print self.error
+                print(self.error, file=sys.stderr)
             
             # warning if there are no analog outputs
             if( not self.boardInfo.nAnalogOut ):
-                print "WARNING: no analog outputs found"
+                print("WARNING: no analog outputs found", file=sys.stderr)
             
             # initialize card
             self.wasco.wasco_openBoard( byref(self.boardId), self.boardInfo.pBoardName)
@@ -61,8 +64,10 @@ class Wasco(object):
 
         except (OSError, ImportError, BaseException):
             if self.dummy is False:
-                print('''########## WARNING ##########
-                        Cannot load wasco.dll. Creating wasco dummy!''')
+                print('''
+                ########## WARNING ##########
+                Cannot load wasco.dll. Creating wasco dummy!
+                ''', file=sys.stderr)
             self.boardId = c_int(1)
             self.error = c_ulong()
             self.boardInfo = constants.WascoBoardInfo()
